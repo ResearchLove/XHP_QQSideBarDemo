20ec9e5b0c3530b938aeb0f79e35c0b22b0c64e1
ResearchLove <15295529134@163.com>
2016-07-11 16:30:18 +0800

    Initial Commit

---
+++ XHP_QQSideBarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHP_QQSideBarDemo
+//
+//  Created by xiaohaiping on 16/7/11.
+//  Copyright © 2016年 HaoHeHealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
